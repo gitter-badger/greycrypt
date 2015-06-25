4be60f02be296297cd9f9ffff9367522fbcc6b1e
John Quigley <jmquigs@gmail.com>
2015-06-25 10:14:49 -0400

    Config: move into module

---
+++ src/config.rs
@@ -0,0 +1,75 @@
+use std::collections::BTreeMap;
+use std::path::{PathBuf};
+use std::fs::{PathExt};
+
+extern crate toml;
+
+use util;
+
+pub struct SyncConfig {
+    raw_toml: BTreeMap<String, toml::Value>,
+    sync_dir: String
+}
+
+pub fn parse() -> SyncConfig {
+    let toml = util::load_toml_file(&"mapping.toml".to_string());
+
+    // verify config
+
+    // host name mapping must exist
+    let hn = util::get_hostname();
+    let hn_key = format!("Machine_{}", hn);
+    let (sync_dir, mapping) = {
+        let mval = toml.get(&hn_key);
+        //println!("{:?}: '{:?}' -> {:?}",toml,&hn_key,mval);
+        let hn_config = match mval {
+            None => { panic!("No hostname config found, cannot continue: {}", hn_key) },
+            Some(c) => c
+        };
+        let hn_config = match hn_config.as_table() {
+            None => { panic!("Hostname config must be a table") },
+            Some(c) => c
+        };
+        let sync_dir = match hn_config.get(&"SyncDir".to_string()) {
+            None => { panic!("No SyncDir specified for host") },
+            Some (sd) => {
+                let sd = sd.as_str().unwrap().to_string();
+                let pp = PathBuf::from(&sd);
+                if !pp.is_dir() {
+                    panic!("Sync directory does not exist: {}", sd);
+                }
+                sd
+            }
+        };
+
+        let mapping = match hn_config.get(&"Mapping".to_string()) {
+            None => { panic!("No mapping for for host") },
+            Some(m) => {
+                match m.as_table() {
+                    None => { panic!("Hostname mapping must be a table") },
+                    Some(m) => {
+                        let map_count = m.len();
+                        if map_count == 0 {
+                            panic!("No mapping entries found for host");
+                        } else {
+                            println!("Found {} mapping entries for this host", map_count);
+                        }
+                        m
+                    }
+                }
+            }
+        };
+
+        //println!("{:?}",mapping);
+
+        (sync_dir, mapping)
+    };
+
+    let c = SyncConfig {
+        raw_toml: toml.clone(),
+        sync_dir: sync_dir
+    };
+
+    println!("SyncDir: {:?}", c.sync_dir);
+    c
+}
