e01ea05dc76e850a44ce2fb09336e9090ce86bca
John Quigley <jmquigs@gmail.com>
2015-07-08 19:51:18 -0400

    Feat(trash): add windows trash code

--- src/trash.rs
+++ src/trash.rs
@@ -1,5 +1,33 @@
-use std::ffi::CString;
 use std::ptr;
+use std::ffi::OsStr;
+
+#[cfg(target_os = "windows")]
+use std::os::windows::ffi::OsStrExt;
+
+#[cfg(target_os = "macos")]
+use std::ffi::CString;
+
+#[cfg(target_os = "windows")]
+extern crate winapi;
+
+#[cfg(target_os = "windows")]
+#[repr(C)]
+struct SHFILEOPSTRUCTW {
+    hwnd: winapi::HWND,
+    w_func: winapi::UINT,
+    p_from: winapi::LPCWSTR,
+    p_to: winapi::LPCWSTR,
+    f_flags: u32,
+    f_any_operations_aborted: winapi::BOOL,
+    h_name_mappings: winapi::LPVOID,
+    lpsz_progress_title: winapi::LPCWSTR
+}
+
+#[cfg(target_os = "windows")]
+#[link(name = "shell32")]
+extern "stdcall" {
+    fn SHFileOperationW(lp_file_op: *mut SHFILEOPSTRUCTW) -> i32;
+}
 
 #[cfg(target_os = "macos")]
 #[repr(C)]
@@ -15,6 +43,37 @@ extern {
     // API, but its probably already deprecated.
     fn FSPathMakeRefWithOptions(path: *const i8, opts: i32, fsref: *mut FSRef, is_directory: *mut i32) -> i32; // TODO: what is "Boolean"? "OsStatus" is intlike?
     fn FSMoveObjectToTrashSync(source: *mut FSRef, target: *mut FSRef, opts: i32) -> i32;
+}
+
+#[cfg(target_os = "windows")]
+pub fn send_to_trash(f:&str) -> Result<(),String> {
+    const FO_DELETE:winapi::UINT = 3;
+    const FOF_SILENT:u32 = 4;
+    const FOF_NOCONFIRMATION:u32 = 16;
+    const FOF_ALLOWUNDO:u32 = 64;
+    const FOF_NOERRORUI:u32 = 1024;
+
+    let path:Vec<u16> = OsStr::new(f).encode_wide().chain(Some(0)).collect::<Vec<_>>();
+
+    let mut fileop = SHFILEOPSTRUCTW {
+        hwnd: ptr::null_mut(),
+        w_func: FO_DELETE,
+        p_from: path.as_ptr(),
+        p_to: ptr::null_mut(),
+        f_flags: FOF_ALLOWUNDO | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT,
+        f_any_operations_aborted: 0,
+        h_name_mappings: ptr::null_mut(),
+        lpsz_progress_title: ptr::null()
+    };
+
+    let res = unsafe {
+        SHFileOperationW(&mut fileop)
+    };
+    if res != 0 {
+        return Err(format!("Failed to send file to recycle bin: {}; SHFileOperationW code: {}", f, res));
+    } else {
+        return Ok(())
+    }
 }
 
 #[cfg(target_os = "macos")]
@@ -38,13 +97,6 @@ pub fn send_to_trash(f:&str) -> Result<(),String> {
             return Err(format!("Failed to move file to trash: {}; FSMoveObjectToTrashSync code: {}", f, res));
         }
         Ok(())
-}
-
-pub fn do_something() {
-    match send_to_trash("/Users/john/Documents/AnotherGreyGryptTestDir/atestfileMINE.txt") {
-        Err(e) => panic!("{}", e),
-        Ok(_) => ()
-    }
 }
 
 #[cfg(test)]
@@ -71,6 +123,13 @@ mod tests {
         pb
     }
 
+    #[cfg(target_os = "windows")]
+    pub fn get_trash_path(f:&PathBuf) -> PathBuf {
+        // not easily possible on windows, since the RB isn't a real folder
+        //PathBuf::new()
+        panic!("I can't do that, Dave");
+    }
+
     pub fn is_in_trash(f:&PathBuf) -> bool {
         get_trash_path(&f).is_file()
     }
@@ -91,7 +150,10 @@ mod tests {
         testpath.push("testdata");
         testpath.push("Afilethatwillbetrashed.bythetrashmodule.trashme");
 
-        remove_trash_file(&get_trash_path(&testpath));
+        // can't really clean out RB on windows
+        if !cfg!(target_os = "windows") {
+            remove_trash_file(&get_trash_path(&testpath));
+        }
 
         {
             let mut f = match File::create(&testpath.to_str().unwrap()) {
@@ -111,8 +173,12 @@ mod tests {
         }
 
         assert!(!testpath.is_file());
-        assert!(is_in_trash(&testpath));
-        remove_trash_file(&get_trash_path(&testpath));
-        assert!(!is_in_trash(&testpath));
+
+        if !cfg!(target_os = "windows") {
+            assert!(is_in_trash(&testpath));
+            remove_trash_file(&get_trash_path(&testpath));
+            assert!(!is_in_trash(&testpath));
+        }
+
     }
 }
