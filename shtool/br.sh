5256a5d1bc7e90592a94a11952454f910f0f4ddf
John Quigley <jmquigs@gmail.com>
2015-07-25 11:37:30 -0400

    Chore(shtool): fix ctrl-c not killing the process on cygwin/msys in "br.sh"

--- shtool/br.sh
+++ shtool/br.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+uname=$(uname)
+
+# these guys need help killing the process on CTRL-C...and the taskkill command varies slightly
+if [[ $uname == *"CYGWIN"* ]] 
+then
+	trap "ps -W | grep -i grey_crypt | grep -i debug | awk '{print \$1}' | xargs -i taskkill /f /pid {}" EXIT
+fi
+if [[ $uname == *"MSYS"* ]]
+then
+	trap "ps -W | grep -i grey_crypt | grep -i debug | awk '{print \$1}' | xargs -i taskkill -f -pid {}" EXIT
+fi
+
 clear
 export RUST_BACKTRACE=1 
-cargo run --verbose
+cargo run --verbose 
+
