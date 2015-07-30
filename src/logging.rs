20e9e45bee2083792339633d9fc7a90ef512f5a7
John Quigley <jmquigs@gmail.com>
2015-07-30 17:48:57 -0400

    Fix(logging): don't re-init logging if already initialized

--- src/logging.rs
+++ src/logging.rs
@@ -32,7 +32,17 @@ impl LoggerUtil {
 }
 
 impl SimpleLogger {
-    pub fn init() -> Result<LoggerUtil, log::SetLoggerError> {
+    pub fn init() -> Result<LoggerUtil, log::SetLoggerError> {   
+        // The logger must only be initialized once per process, 
+        // else it panics.  This Effs up the tests.  So here 
+        // is a crude check that makes sure we init only once.
+        // Can init LoggerUtil as much as we want, though.
+        if log_enabled!(log::LogLevel::Info) {             
+            return Ok(LoggerUtil {
+                warnonce_messages: HashSet::new()
+            })
+        }
+        
         let res = log::set_logger(|max_log_level| {
             max_log_level.set(log::LogLevelFilter::Info);
             
